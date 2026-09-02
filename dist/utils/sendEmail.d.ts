e5b6379037f9385b545e2f2244ce12c2879bd5dc
Bilal-Afzal-Bhatti <bilalafzalbhatti@gmail.com>
2026-09-02 22:04:00 +0500

    ADDED THE PATH IN PETS FILE

--- dist/utils/sendEmail.d.ts
+++
@@ -1,8 +0,0 @@
-interface EmailOptions {
-    email: string;
-    subject: string;
-    message: string;
-}
-export declare const sendEmail: (options: EmailOptions) => Promise<void>;
-export default sendEmail;
-//# sourceMappingURL=sendEmail.d.ts.map
